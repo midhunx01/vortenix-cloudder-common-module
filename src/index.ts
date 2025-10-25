8a9a6b2ef0c0e3f47110c4836a1fa40de42c1808
midhunx01 <midhunxda.123@gmail.com>
2025-10-25 14:16:53 +0530

    fix

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from "./events/base-event.js";
 export * from "./events/base-listener.js";
 export * from "./events/base-publisher.js";
 export * from "./events/subjects.js";
+export * from "./events/account-created-event.js";
